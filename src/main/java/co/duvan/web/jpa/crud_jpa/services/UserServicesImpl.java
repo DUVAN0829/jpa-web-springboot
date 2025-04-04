aea8814e026db96918320567fc89c7b15ddc2470
Mac User <macuser.@Macs-MBP.bbrouter>
2025-04-03 21:23:50 -0500

    añade directorio 'security'. Refactoriza 'UserServicesImpl'

--- src/main/java/co/duvan/web/jpa/crud_jpa/services/UserServicesImpl.java
+++ src/main/java/co/duvan/web/jpa/crud_jpa/services/UserServicesImpl.java
@@ -3,10 +3,12 @@ package co.duvan.web.jpa.crud_jpa.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import co.duvan.web.jpa.crud_jpa.entities.User;
+import co.duvan.web.jpa.crud_jpa.repositories.RoleRepository;
 import co.duvan.web.jpa.crud_jpa.repositories.UserRepository;
 
 @Service
@@ -15,6 +17,12 @@ public class UserServicesImpl implements UserServices {
     // *Vars */
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional(readOnly = true)
