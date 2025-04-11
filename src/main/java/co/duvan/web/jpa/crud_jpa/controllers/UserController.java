93efb5e805522d4c705e4f9311b8d6de6bede9eb
Duglas <macuser.@Macs-MBP.bbrouter>
2025-04-10 20:57:42 -0500

    Refactoriza 'UserServicesImpl' y añade controlador 'Usercontroller'

---
+++ src/main/java/co/duvan/web/jpa/crud_jpa/controllers/UserController.java
@@ -0,0 +1,12 @@
+package co.duvan.web.jpa.crud_jpa.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    
+
+}
--- src/main/java/co/duvan/web/jpa/crud_jpa/services/UserServicesImpl.java
+++ src/main/java/co/duvan/web/jpa/crud_jpa/services/UserServicesImpl.java
@@ -1,12 +1,15 @@
 package co.duvan.web.jpa.crud_jpa.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import co.duvan.web.jpa.crud_jpa.entities.Role;
 import co.duvan.web.jpa.crud_jpa.entities.User;
 import co.duvan.web.jpa.crud_jpa.repositories.RoleRepository;
 import co.duvan.web.jpa.crud_jpa.repositories.UserRepository;
@@ -16,7 +19,7 @@ public class UserServicesImpl implements UserServices {
 
     // *Vars */
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
 
     @Autowired
     private RoleRepository roleRepository;
@@ -24,16 +27,34 @@ public class UserServicesImpl implements UserServices {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    // *Methods */
     @Override
     @Transactional(readOnly = true)
     public List<User> findAll() {
-        return (List<User>) repository.findAll();
+        return (List<User>) userRepository.findAll();
     }
 
     @Override
     @Transactional
     public User save(User user) {
-        return repository.save(user);
+
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+
+        List<Role> roles = new ArrayList<>();
+
+        optionalRoleUser.ifPresent(roles::add);
+
+        if (user.isAdmin()) {
+
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword())); // *Codifica el password */
+
+        return userRepository.save(user);
     }
 
 }
